e5120f0a8ede0b061565919145c08612af7cc56b
daegyo <ldk7024@naver.com>
2023-01-30 15:21:41 +0900

    java 파일 추가

---
+++ daegyo.java
@@ -0,0 +1,16 @@
+public class Daegyo {
+
+	public static void main(String[] args) {
+		
+		args = new String[3]; //3개의 저장공간을 가진 배열 초기화
+		args[0] = "안녕"; //배열의 첫번째 공간에 데이터입력
+		args[1] = "반가워"; //배열의 두번째 공간에 데이터입력
+		args[2] = "대교라고해"; //배열의 세번째 공간에 데이터입력
+		
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		
+	}
+
+}
